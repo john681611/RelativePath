1abdf7f43ff5ba0ded40eebf98485696a0ba1b22
john681611 <john.harvey@marks-and-spencer.com>
2018-02-23 16:39:55 +0000

    Basic copy relative path.

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
-'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import copypaste = require('copy-paste');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,11 +14,38 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.copyRelPath', (res, files) => {
         // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        // Display a message box to the use
+        if(files.length){
+            const workspace = vscode.workspace.getWorkspaceFolder(files[0]);
+            if(workspace){
+                const regex = new RegExp(workspace.uri.path,'g');
+                const lineDelimiter = '\n';
+                const relPath =files.map((element: vscode.Uri) => element.path.replace(regex,'')).join(lineDelimiter);
+                console.log(relPath);
+                copypaste.copy(relPath, res => {
+                if(res !== null) {
+                        vscode.window.showErrorMessage('Could not copy: '+res);
+                    }
+                });
+            }else{
+                vscode.window.showErrorMessage('Cant find workspace: '+res);
+            }
+        } else {
+            vscode.window.showErrorMessage('Could not copy: '+res);
+        }
+        // let relPath = '';
+        // files.array.forEach(element => {
+        //     relPath += element.path.replace(regex,'') + '\n';
+        // });
+        // copypaste.copy(relPath, res => {
+        //     if(res != null) {
+        //         // something went wrong...
+        //         vscode.window.showErrorMessage('Could not copy: '+res);
+        //     }
+        // });
     });
 
     context.subscriptions.push(disposable);
