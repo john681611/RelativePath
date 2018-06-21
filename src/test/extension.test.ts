00d3f76e3a04d0337e73b3e75649243c86d9c2a0
john681611 <john.harvey@marks-and-spencer.com>
2018-06-21 14:41:53 +0100

    Inital focused path implementation

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -4,7 +4,15 @@
 
 // suite("Extension Tests", () => {
 
-//     test("Regesters a command", () => {
-//         //Need To figure this out
-//     });
-// });
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+
+// Defines a Mocha test suite to group tests of similar kind together
+suite("Extension Tests", () => {
+
+    // Defines a Mocha unit test
+    test("Something 1", () => {
+        assert.equal(-1, [1, 2, 3].indexOf(5));
+        assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
+});
